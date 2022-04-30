866526cdafda9c4732b9951cee6a275ebfca03e3
Zihan Jin <72365265+jin-zihan@users.noreply.github.com>
2022-05-01 03:02:33 +0800

    Update 01-data_cleaning.R

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,35 +1,67 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
-# License: MIT
+# Purpose: Clean the survey data downloaded from U.S. Department of Education  [https://collegescorecard.ed.gov/data]
+# Author: Zihan Jin
+# Data: 30 April 2022
+# Contact: zihan.jin@mail.utoronto.ca
 # Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
-
+# - Need to have downloaded the College Scorecard data and saved it to inputs/data
 
 #### Workspace setup ####
 # Use R Projects, not setwd().
-library(haven)
 library(tidyverse)
+library(reshape2)
+library(patchwork)
+library(car)
 # Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
+college <- read_csv("Most-Recent-Cohorts-Institution.csv")
+              
+# convert data to correct type corrosponding to its real meaning
+college$SAT_AVG <- as.numeric(college$SAT_AVG)
+college$UGDS_MEN <- as.numeric(college$UGDS_MEN)
+college$UGDS_WOMEN <- as.numeric(college$UGDS_WOMEN)
+college$UGDS_WHITE <- as.numeric(college$UGDS_WHITE)
+college$UGDS_BLACK <- as.numeric(college$UGDS_BLACK)
+college$UGDS_HISP <- as.numeric(college$UGDS_HISP)
+college$UGDS_ASIAN <- as.numeric(college$UGDS_ASIAN)
+college$UGDS_AIAN <- as.numeric(college$UGDS_AIAN)
+college$UGDS_NHPI <- as.numeric(college$UGDS_NHPI)
+college$C150_4_WHITE <- as.numeric(college$C150_4_WHITE)
+college$C150_4_BLACK <- as.numeric(college$C150_4_BLACK)
+college$C150_4_HISP <- as.numeric(college$C150_4_HISP)
+college$C150_4_ASIAN <- as.numeric(college$C150_4_ASIAN)
+college$C150_4_AIAN <- as.numeric(college$C150_4_AIAN)
+college$C150_4_NHPI <- as.numeric(college$C150_4_NHPI)
+college$TUITIONFEE_IN <- as.numeric(college$TUITIONFEE_IN)
+college$TUITIONFEE_OUT <- as.numeric(college$TUITIONFEE_OUT)
+college$MD_EARN_WNE_P6 <- as.numeric(college$MD_EARN_WNE_P6)
+college$MD_EARN_WNE_P8 <- as.numeric(college$MD_EARN_WNE_P8)
+college$MD_EARN_WNE_P10 <- as.numeric(college$MD_EARN_WNE_P10)
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
-         
+# drop missing value
 
-#### What's next? ####
+college <- college %>%
+  filter(!is.na(SAT_AVG) & !is.na(C150_4_WHITE) & !is.na(C150_4_BLACK) & !is.na(C150_4_HISP) & 
+         !is.na(C150_4_ASIAN) & !is.na(C150_4_AIAN) & !is.na(C150_4_NHPI) & !is.na(TUITIONFEE_IN ) & 
+         !is.na(TUITIONFEE_OUT) & !is.na(UGDS_MEN) & !is.na(UGDS_WOMEN) & !is.na(UGDS_WHITE) & 
+         !is.na(UGDS_BLACK) & !is.na(UGDS_HISP) & !is.na(UGDS_ASIAN) & !is.na(UGDS_AIAN) & !is.na(UGDS_NHPI) & 
+         !is.na(MD_EARN_WNE_P6) & !is.na(MD_EARN_WNE_P8) & !is.na(MD_EARN_WNE_P10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
